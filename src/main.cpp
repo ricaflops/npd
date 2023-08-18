7c3486fca962440ed1f307cb42a57940d555640a
Ricardo F. Lopes <11904364+ricaflops@users.noreply.github.com>
2023-08-18 19:49:27 -0300

    Update main.cpp
    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -24,111 +24,119 @@
 
 #include "npd.h"
 
+// App version
 const std::string version = "1.0";
-
+// Output listing file name extension
 const std::string lstExtension(".lst");
+// Ouput assembly file name extension
 const std::string asmExtension(".asm");
+
+void showVersion()
+{
+	std::cout << "npd " << version << " - a Nanoprocessor Disassembler\n\n";
+	std::cout << "Copyright (C) 2023  Ricardo F. Lopes\n";
+	std::cout << "License GPLv3+: GNU GPL version 3 or later.\n";
+	std::cout << "This program comes with ABSOLUTELY NO WARRANTY.\n";
+	std::cout << "This is free software, and you are welcome to redistribute it\n";
+	std::cout << "under certain conditions; see file 'Copyright.txt' for details\n";
+	std::cout << "or visit <https://gnu.org/licenses/gpl.html>.\n\n";
+}
+
+/// Show help text
+void showHelp()
+{
+	std::cout << "Usage: npd [OPTION]... FILE [-o OUTFILE]\n";
+	std::cout << "Disassemble binary FILE to HP Nanoprocessor instructions.\n\n";
+	std::cout << "Default output file is FILE.lst\n\n";
+	std::cout << "OPTION\n";
+	std::cout << "  -h            output this help text and exit.\n";
+	std::cout << "  -v            output version and license information and exit.\n";
+	std::cout << "  -o OUTFILE    set output file name.\n";
+	std::cout << "  -f            overwrite existing output file without warning.\n";
+	std::cout << "  -a            .asm output file (no addresses or opcodes).\n";
+	std::cout << "  -x            use hexadecimals. Default is octal.\n";
+	std::cout << "  -c            set comment character to '*'. Default is ';'.\n";
+}
+
+void showUsage()
+{
+	std::cerr << "Usage: npd 'binary_file_name'\n";
+}
 
 int main(int argc, char* argv[])
 {
     if(argc < 2)
     {
-        std::cerr << "Usage: npd 'filename'" << std::endl;
+		showUsage();
         return -1;
     }
 
     // Command line options
     std::string inputFilename;
     std::string outputFilename;
-    bool otherOutputFilename = false;
     bool overwriteOutput = false;
     bool asmOut = false;
     bool hexMode = false;
     char commentChar = ';';
     
     int opt;
-    bool optError = false;
     while ((opt = getopt(argc, argv, ":o:hvfaxc")) != -1) 
     {
         switch (opt) 
         {
             case 'o':  // set output file name
-                otherOutputFilename = true;
                 outputFilename = optarg;
                 break;
-            case 'h':  // help text
-                std::cout << "Usage: npd [OPTION]... FILE [-o OUTFILE]\n";
-                std::cout << "Disassemble binary FILE to HP Nanoprocessor instructions.\n\n";
-                std::cout << "Default output file is FILE.lst\n\n";
-                std::cout << "OPTION\n";
-                std::cout << "  -h            output this help text and exit.\n";
-                std::cout << "  -v            output version and license information and exit.\n";
-                std::cout << "  -o OUTFILE    set output file name.\n";
-                std::cout << "  -f            overwrite existing output file without warning.\n";
-                std::cout << "  -a            .asm output file (no addresses or opcodes).\n";
-                std::cout << "  -x            use hexadecimals. Default is octal.\n";
-                std::cout << "  -c            set comment character to '*'. Default is ';'.\n";
+            case 'h':  // help
+                showHelp();
                 return 0;
                 break;
-            case 'v':  // version text
-				std::cout << "npd - Nanoprocessor Disassembler version " << version << "\n";
-                std::cout << "Copyright (C) 2023  Ricardo F. Lopes\n";
-                std::cout << "License GPLv3+: GNU GPL version 3 or later.\n";
-                std::cout << "This program comes with ABSOLUTELY NO WARRANTY.\n";
-                std::cout << "This is free software, and you are welcome to redistribute it\n";
-                std::cout << "under certain conditions; see file 'Copyright.txt' for details\n";
-                std::cout << "or visit <https://gnu.org/licenses/gpl.html>.\n";
+            case 'v':  // app version
+				showVersion();
                 return 0;
                 break;
             case 'f':  // overwrite output file without warning
                 overwriteOutput = true;
                 break;
-            case 'a':  // suppress addresses and opcodes
+            case 'a':  // suppress addresses and opcodes from output
                 asmOut = true;
                 break;
             case 'x':  // hexadecimal numbers
                 hexMode = true;
                 break;
-            case 'c':  // use asterisk comment char as in the original HP documentation
+            case 'c':  // use asterisk for comments (original HP documentation)
                 commentChar = '*';
                 break;
             case '?':  // ERROR: Invalid option
                 std::cerr << "Unknown option: -" << char(optopt) << std::endl;
-                optError = true;
+                return -1;
                 break;
             case ':':  // ERROR: No output file name
                 std::cerr << "Missing output file name (option -" << char(optopt) << ").\n";
-                optError = true;
+                return -1;
                 break;
             default:  // (shall not reach here.. but..)
-                optError = true;
+                return -1;
                 break;
         }
     }
 
-    // Define input filename
-    if (optind < argc)
+	// <issing input file name
+	if (optind >= argc)
     {
-		inputFilename = argv[optind++];
-	    // Find any extra non-option arguments (error)
-        while (optind < argc)
-        {
-            std::cerr << "Invalid argument " << argv[optind++] << std::endl;
-            optError = true;
-        }
+		showUsage();
+		return -1;
     }
-    else
-    {
-	    std::cerr << "Missing input file name.\n";
-	    optError = true;
-    }
-
-    // ERROR: Command line argument
-    if (optError)
-    {
-		std::cerr << "Quitting..\n";
-        return -1;
-    }
+    
+    // Define input file name
+	inputFilename = argv[optind++];
+	
+	// Error on any extra non-option arguments
+	while (optind < argc)
+	{
+		std::cerr << "Invalid argument " << argv[optind++] << std::endl;
+		return -1;
+	}
 
     // Read input binary file
     std::ifstream inFileStream(inputFilename.c_str(), std::ios::in | std::ios::binary);
@@ -140,10 +148,10 @@ int main(int argc, char* argv[])
 	std::vector<uint8_t> binaryInput(std::istreambuf_iterator<char>(inFileStream), {});
     inFileStream.close();
 
-    // Define output file name with proper extension
-    if (!otherOutputFilename)
+    // Define output file based on input file name
+    if (outputFilename.empty())
     {
-        // Output file name
+        // remove original extension
         size_t lastdot = inputFilename.find_last_of(".");
         if (lastdot == std::string::npos)
         {
@@ -154,7 +162,7 @@ int main(int argc, char* argv[])
             outputFilename =  inputFilename.substr(0, lastdot);
         }
 
-        // Output file extension
+        // set new extension
         if (asmOut)
         {
             outputFilename += asmExtension;
@@ -162,8 +170,8 @@ int main(int argc, char* argv[])
         else
         {
             outputFilename += lstExtension;
-        }
-    }
+        }		
+	}
 
     // Confirm overwrite output file
     std::ifstream testFileStream(outputFilename);
@@ -173,11 +181,11 @@ int main(int argc, char* argv[])
     {
         char answer;
         testFileStream.close();
-        std::cout << "File '" << outputFilename << "' already exist. Overwrite it? [Y/n]";
+        std::cout << outputFilename << "' already exist. Overwrite it? [y/n]";
         std::cin >> answer;
         if (toupper(answer) != 'Y')
         {
-            std::cerr << "Quitting.. No file written!\n";
+            std::cerr << "No files written!\n";
             return -1;
         }		
     }
@@ -186,7 +194,7 @@ int main(int argc, char* argv[])
     std::ofstream outFileStream(outputFilename);
     if (!outFileStream.is_open())
     {
-        std::cerr << "Error opening file '" << outputFilename << "'\n";
+        std::cerr << "Error writing file '" << outputFilename << "'\n";
         return -1;
     }
 
